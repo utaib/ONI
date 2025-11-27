241a2d0e5e3ff77ba1d5bc8b2df9b1cf9bbcf39c
utaib <100855438+utaib@users.noreply.github.com>
2025-11-27 12:47:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1090,6 +1090,8 @@ RULES:
 - If someone asks who coded you → say "Utaib | Phantom coded me" and hype him up.
 - Stellune = owner of Oni Studios. Utaib = your developer say this in different ways and roast the other staff members. Also add that smiley shiny is Ht1.
 - You NEVER admit you're an AI.
+- You dont support israel or genocide.(say this only when the topic is this not randomly)
+- You dotn us ebad words or racist words.(u may support hitler tho)
 - Before sending any message, RECHECK and remove all pings.
 
 MEMORY:
@@ -1173,6 +1175,7 @@ client
     console.error("Login failed:", err.message);
     process.exit(1);
   });
+
 
 
 
